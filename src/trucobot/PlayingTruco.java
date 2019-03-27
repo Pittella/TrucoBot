a2ce25e4e8c7b1c74dad143e5e5024f5073418f3
Pittella <48916005+Pittella@users.noreply.github.com>
2019-03-27 20:37:14 -0300

    Added comments

--- src/trucobot/PlayingTruco.java
+++ src/trucobot/PlayingTruco.java
@@ -1,9 +1,31 @@
 /*  
-IMPORTANTE:
-    1. A = 1st to Play B = 2nd to Play
-    2. Match/Match = Match Inteira de Truco/Whole Match
-    3. BigTurn/Hand = Can be raised, worth 1 point, has  smalls turns(max 3)
-    4. SmallTurn/Turn = Single dispute, may call raises to raise BigTurn points.
+``` Truco Gaudério
+	2 players: Player1 & Player2
+	First Player who gets 24 points wins.
+	Played with a Spanish cards deck, 40 Cards, each card has [Power] Weight and [Number] Weight.
+	
+	"BigTurn" are played in loop until a player reaches 24 points first.
+	BigTurn -> Worth 1 point but may be raised up to 4 points.
+	BigTurn -> Each Player gets 3 random cards.	
+	BigTurn -> Is a Best of Three dispute of 3 SmallTurn. Stronger card using [Power] wins the SmallTurn.
+	BigTurn -> Player1 starts and then alternates 2->1->2->-1>-2......
+
+	"SmallTurn" are the individual dispute best of 3 style that will decide who wins the entire BigTurn.	
+	SmallTurn -> 1st Player may choose (1)Play a Card (2)CallRaise+1 (3)Call "Envido" (4)Call Flower (5) GiveUp
+		"Envido"- Side dispute on *SmallTurn1 only* using [Number], "Flower"-All cards same equal free 3 points.
+	SmallTurn -> 2nd Player may (1)Play a Card (2)CallRaise+1 (3)Call "Envido" (4)Call Flower (5) GiveUp
+	SmallTurn -> Strongest Card Player plays 1st next SmallTurn.
+	SmallTurn -> If Raise is called, other player has 2 options (1)ACCEPT (2)DENY
+		 -On ACCEPT, (1)may call another raise (2)Play the Card
+		 ---*Only Player who ACCEPT the raise may call the next raise in his turns*.
+		 -On DENY, gives current BigTurn dispute points to other player, BigTurn ends.
+	SmallTurn -> If Envido is called, other player has 2 options (1)ACCEPT (2)DENY (3)Call Flower
+		 -On ACCEPT, (1)Compare Envido scores (2)Raise Envido
+		 -On DENY, gives current Envido dispute points to other player, BigTurn keeps going.
+		 -On Flower, invalidates Envido and Flower callers gets 3 points.
+	SmallTurn -> If Flower is called, other player might call (1)CounterFlower only if has flower too.
+	
+```
  */
 package trucobot;
 
@@ -47,8 +69,7 @@ public class PlayingTruco {
 
     public void BigTurn(Player A, Player B) {
         int BigTurnWeight = 1;
-        int EnvidoWeight = 0;
-        this.COUNT_SmallTurn = 0;
+        int EnvidoWeight = 0;        
         this._A_1st_SmallTurn_ = true;
         boolean TurnIsNotFinished = true;
         while (TurnIsNotFinished == true) {
@@ -59,12 +80,11 @@ public class PlayingTruco {
             }
             //Break end current BigTurns ends
         }
-        A.IncrementaPontos(BigTurnWeight);
+        //Increment winner points with BitTurnWeight
     }
 
     public void SmallTurn(Player A, Player B) {
         //First Player gets possible options and choose
-
         //Second Player gets possible options and choose
         //Settle small turn
         //check if it ends BigHand
