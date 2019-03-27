67b25da748cf29804c1260a2cad5b3a6e8cdf99a
Pittella <48916005+Pittella@users.noreply.github.com>
2019-03-26 23:28:23 -0300

    1234

--- src/trucobot/PlayingTruco.java
+++ src/trucobot/PlayingTruco.java
@@ -11,15 +11,16 @@ package trucobot;
 
 public class PlayingTruco {
 
-    Deck Baralho = new Deck();    
+    Deck Baralho = new Deck();
 
-    boolean WhileVar = true;
-    boolean A_PRIMEIRO_MAO = true, A_PRIMEIRO_RODADA = true;
-    boolean TRUCO = false, RETRUCO = false, VALE4 = false;
+    boolean WhileVar = true; 
+    boolean A_PRIMEIRO_MAO = true, A_PRIMEIRO_RODADA = true; 
+    boolean TRUCO = false, RETRUCO = false, VALE4 = false; 
     boolean ENVIDO = false, REAL_ENVIDO = false, FALTA_ENVIDO = false;
     boolean EMPATE_PRIMEIRA = false, EMPATE_SEGUNDA = false, EMPATE_TERCEIRA = false;
-    boolean PRIMEIRA_RODADA;
+    boolean PRIMEIRA_RODADA = true;
     boolean A_GANHOU_MAO = false, B_GANHOU_MAO = false, A_GANHOU_ENVIDO = false, B_GANHOU_ENVIDO = false;
+    int COUNT_MAO, COUNT_RODADA; 
 
     public void DealCards(Hand A, Hand B) {
         Baralho.ShuffleDeckArray();
@@ -31,16 +32,16 @@ public class PlayingTruco {
         A.setPontos(0);
         B.setPontos(0);
         Baralho.BuildDeckArray();
-        System.out.printf("Partida() Pts de %d:%s Pts de %d=%s\n", A.getPontos(), A.getNome(), B.getPontos(), B.getNome());
+        System.out.printf("Começa a Partida, Pts de %d:%s Pts de %d=%s\n", A.getPontos(), A.getNome(), B.getPontos(), B.getNome());
         while (WhileVar) {
             //System.out.printf("WHILE PARTIDA Pts de %d:%s Pts de %d=%s\n", A.getPontos(), A.getNome(), B.getPontos(), B.getNome());
             DealCards(A.getPlayerHand(), B.getPlayerHand());
             if (this.A_PRIMEIRO_MAO == true) {
-                System.out.println(A.getNome() + " joga primeiro");
+                System.out.println(A.getNome() + " eh Mao");
                 Mao(A, B);
                 this.A_PRIMEIRO_MAO = false;
             } else {
-                System.out.println(B.getNome() + " joga primeiro");
+                System.out.println(B.getNome() + " eh Mao");
                 Mao(B, A);
                 this.A_PRIMEIRO_MAO = true;
             }
@@ -52,29 +53,45 @@ public class PlayingTruco {
             }
             System.out.printf("FIM WHILE PARTIDA Pts de %d:%s Pts de %d=%s\n", A.getPontos(), A.getNome(), B.getPontos(), B.getNome());
         }
+        System.out.printf("Termina a Partida, Pts de %d:%s Pts de %d=%s\n", A.getPontos(), A.getNome(), B.getPontos(), B.getNome());
 
     }
 
     public void Mao(Player A, Player B) {
-        int a = 1; //Valor da Mao pode ser aumentada
-        int b = 0; //Valor de Envido        
+        int ValorMao = 12; //Valor da Mao pode ser aumentada
+        int ValorEnvido = 0; //Valor de Envido        
+        this.COUNT_RODADA = 0;
         this.A_PRIMEIRO_RODADA = true;
         while (WhileVar) {
             if (this.A_PRIMEIRO_RODADA == true) {
+                System.out.println("joga primeiro na rodada:"+A.getNome());
                 Rodada(A, B);
             } else {
+                System.out.println("joga primeiro na rodada:"+B.getNome());
                 Rodada(B, A);
             }
+            if (COUNT_RODADA >= 3) {
+                break;
+            }
+            COUNT_RODADA = COUNT_RODADA + 1;
         }
-        A.IncrementaPontos(a);
+        A.IncrementaPontos(ValorMao);
         System.out.printf("FIM MAO Pts de %d:%s Pts de %d=%s\n", A.getPontos(), A.getNome(), B.getPontos(), B.getNome());
 
     }
 
-    public void Rodada(Player A, Player B) {        
-        /*OPCOES A:JOGAR 1 CARTA, TRUCO, ENVIDO*/
-        /*OPCOES B:JOGAR 1 CARTA, TRUCO, ENVIDO, RETRUCO, AUMENTAR ENVIDO*/
-
+    public void Rodada(Player A, Player B) {
+        System.out.printf("Rodada Numero: %d\n",this.COUNT_RODADA);
+        System.out.println(A.getNome()+" joga");
+        System.out.println(B.getNome()+" joga");
+        System.out.println("Ver quem ganhou");
+        if (this.A_PRIMEIRO_RODADA == true) {            
+            System.out.println(B.getNome()+" ganhou rodada");
+            this.A_PRIMEIRO_RODADA = false;
+        } else {
+            System.out.println(A.getNome()+" ganhou rodada");
+            this.A_PRIMEIRO_RODADA = true;
+        }        
     }
 
 }
