828a014aac36fe8fa63e6a50911507b29cbd6891
Pittella <48916005+Pittella@users.noreply.github.com>
2019-04-27 02:18:08 -0300

    Falta:retruco,vale4,envidos,flor e quando emparda.

--- src/trucobot/Hand.java
+++ src/trucobot/Hand.java
@@ -3,7 +3,7 @@ package trucobot;
 public class Hand {
 
     public Card[] HandArray = new Card[3];
-    public Card CartaVazia = new Card(0, 0, 0, "Vazia");
+    public Card CartaVazia = new Card(-1, -1, -1, "Vazia");
 
     public void SetCards(Card x, Card y, Card z) {
         this.HandArray[0] = x;
--- src/trucobot/PlayingTruco.java
+++ src/trucobot/PlayingTruco.java
@@ -86,8 +86,10 @@ public class PlayingTruco {
         int PontosDaMaoA = 0;
         int PontosDaMaoB = 0;
 
-        A.setPrimeiroDaRodada(true);
-        B.setPrimeiroDaRodada(false);
+        //A.setPrimeiroDaRodada(true);
+        //B.setPrimeiroDaRodada(false);
+        A.PrimeiroDaRodada = true;
+        B.PrimeiroDaRodada = false;
 
         this.RodadaEmAndamento = true;
 
@@ -101,13 +103,13 @@ public class PlayingTruco {
                 break;
             }
 
-            if (A.PrimeiroDaRodada == true && B.SegundoDaRodada == true) {
+            if (A.PrimeiroDaRodada == true && B.PrimeiroDaRodada == false) {
 
                 //A.setPontos(A.getPontos() + this.PesoDaMao);
                 RodadaDaMao(A, B);
 
                 //this.RodadaEmAndamento = false;
-            } else if (B.PrimeiroDaRodada == true && A.SegundoDaRodada == true) {
+            } else if (B.PrimeiroDaRodada == true && A.PrimeiroDaRodada == false) {
 
                 //B.setPontos(B.getPontos() + this.PesoDaMao);
                 RodadaDaMao(B, A);
@@ -118,19 +120,71 @@ public class PlayingTruco {
             }
 
             if (A.JogouUmaCarta == true && B.JogouUmaCarta == true) {
+                
                 System.out.println("Ambos jogadores jogaram uma carta na rodada #" + CountRodada);
+                
                 if (ContabilizaCartasDaRodada(A.Mesa[CountRodada - 1], B.Mesa[CountRodada - 1], CountRodada) == 1) {
+                    
                     System.out.println(A.getNome() + " vence a rodada");
+                    
                     PontosDaMaoA = PontosDaMaoA + 1;
+                    
+                    A.PrimeiroDaRodada = true;
+                    
+                    B.PrimeiroDaRodada = false;
+                    
+
                 } else if (ContabilizaCartasDaRodada(A.Mesa[CountRodada - 1], B.Mesa[CountRodada - 1], CountRodada) == 2) {
+                    
                     System.out.println(B.getNome() + " vence a rodada");
+                    
                     PontosDaMaoB = PontosDaMaoB + 1;
+                    
+                    B.PrimeiroDaRodada = true;
+                    
+                    A.PrimeiroDaRodada = false;
+                    
+
                 } else {
+                    
                     System.out.println("Empardou a rodada #" + CountRodada);
+                    
+                }
+                
+                if(PontosDaMaoA==2){
+                    
+                    break;
+                    
+                }
+                
+                if(PontosDaMaoB==2){
+                    
+                    break;
+                    
                 }
 
                 this.CountRodada = this.CountRodada + 1;
+                
+                A.JogouUmaCarta = false;
+                
+                B.JogouUmaCarta = false;
+                
             }
+
+        }
+
+        if (PontosDaMaoA > PontosDaMaoB) {
+            
+            System.out.println(A.getNome() + " pontoa");
+            
+            A.setPontos(A.getPontos() + this.PesoDaMao);
+            
+        } else if (PontosDaMaoB > PontosDaMaoA) {
+            
+            System.out.println(B.getNome() + " pontoa");
+            
+            B.setPontos(B.getPontos() + this.PesoDaMao);
+            
         }
         //Increment winner points with BitTurnWeight
     }
@@ -138,12 +192,14 @@ public class PlayingTruco {
     public void RodadaDaMao(Player A, Player B) {
 
         System.out.println("Rodada #" + this.CountRodada + " PesoDaMao:" + this.PesoDaMao);
-        
+
         A.VezDeJogar = true;
         A.ChamouTruco = false;
 
         while (A.VezDeJogar == true) {
+            
             OpcoesDeRodada(A, B);
+            
         }
 
         A.setPrimeiroDaRodada(false);
@@ -155,38 +211,57 @@ public class PlayingTruco {
     public void OpcoesDeRodada(Player A, Player B) {
         int EscolhaDaJogada;
 
-        System.out.println("Jogador " + A.getNome() + " opcoes:");
+        System.out.println("Jogador " + A.getNome() + " opcoes:" + " PesoDaMao:" + this.PesoDaMao);
 
         A.PHand.PrintHand();
 
-        if(A.ChamouTruco==true && this.CountRodada == 1){        
-            
+        if (A.ChamouTruco == true && this.CountRodada == 1) {
+
             System.out.println("1 2 3 ->Cartas 5->Envido 6->Real Envido 7->Falta Envido 8->Flor 9->Exit ");
-            
-        } else if(this.CountRodada == 1){
-            
+
+        } else if (this.CountRodada == 1) {
+
             System.out.println("1 2 3 ->Cartas 4->Truco 5->Envido 6->Real Envido 7->Falta Envido 8->Flor 9->Exit ");
-            
-        }
-        else{
-            
+
+        } else {
+
             System.out.println("1 2 3 ->Cartas 4->Truco 9->Exit ");
-            
+
         }
 
         EscolhaDaJogada = A.GetPlayerInput();
 
         if (EscolhaDaJogada == 1 || EscolhaDaJogada == 2 || EscolhaDaJogada == 3) {
-            A.Mesa[CountRodada - 1] = A.PHand.HandArray[EscolhaDaJogada - 1];
-            A.JogouUmaCarta = true;
-            System.out.println(A.getNome() + " jogou " + A.PHand.HandArray[EscolhaDaJogada - 1].Nome);
-            System.out.println("Passando a vez para outro jogador");
-            A.VezDeJogar = false;
+
+            if (A.PHand.HandArray[EscolhaDaJogada - 1].peso == -1) {
+
+                System.out.println("Carta Vazia Escolha Invalida");
+
+            } else {
+                
+                A.Mesa[CountRodada - 1] = A.PHand.HandArray[EscolhaDaJogada - 1];
+
+                A.PHand.HandArray[EscolhaDaJogada - 1] = A.PHand.CartaVazia;
+
+                A.JogouUmaCarta = true;
+
+                System.out.println(A.getNome() + " jogou " + A.Mesa[CountRodada - 1].Nome);
+
+                System.out.println("Passando a vez para outro jogador");
+
+                A.VezDeJogar = false;
+            }
+
         } else if (EscolhaDaJogada == 4) {
+
             System.out.println(A.getNome() + " chamou truco");
+
             A.ChamouTruco = true;
+
             B.ChamouTruco = false;
+
             ChamouTruco(A, B);
+
         }
 
     }
@@ -195,28 +270,46 @@ public class PlayingTruco {
         int RespostaTruco;
 
         if (A.ChamouTruco == true) {
-            System.out.println(B.getNome() + " aceitas truco? (1)Sim (2)Nao");
+            
+            System.out.println(B.getNome() + " aceitas truco? (1)Sim (2)Nao (3)Sim e Retruco");
+            
             RespostaTruco = B.GetPlayerInput();
+            
             if (RespostaTruco == 1) {
+                
                 System.out.println("Aceitastes");
+                
                 this.PesoDaMao = 2;
+                
             } else if (RespostaTruco == 2) {
+                
                 System.out.println("Recusastes");
+                
                 System.out.println("Fim da rodada");
+                
                 this.RodadaEmAndamento = false;
+                
             }
         }
 
     }
 
     public int ContabilizaCartasDaRodada(Card A, Card B, int Rodada) {
+        
         if (A.peso > B.peso) {
+            
             return 1;
+            
         } else if (A.peso < B.peso) {
+            
             return 2;
+            
         } else {
+            
             return 3;
+            
         }
+        
     }
 
 }
